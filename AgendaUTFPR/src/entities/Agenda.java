36e409b67495eabc5a802ec1d3e7b3543dc33a5d
João Gabriel Loureiro <146102111+LRGabrieldev@users.noreply.github.com>
2024-06-18 17:43:50 -0300

    Update Agenda.java

--- AgendaUTFPR/src/entities/Agenda.java
+++ AgendaUTFPR/src/entities/Agenda.java
@@ -9,6 +9,46 @@ public class Agenda {
 	private String descricao;
 	
 	private Compromisso[] compromisso;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Compromisso[] getCompromisso() {
+		return compromisso;
+	}
+
+	public void setCompromisso(Compromisso[] compromisso) {
+		this.compromisso = compromisso;
+	}
 	
 
 }
