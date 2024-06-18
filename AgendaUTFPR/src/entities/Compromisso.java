f933bea442ba9450026e5f07cae9be11192d7dee
João Gabriel Loureiro <146102111+LRGabrieldev@users.noreply.github.com>
2024-06-18 17:45:15 -0300

    Update Compromisso.java
    Getter's e Setter's adicionados

--- AgendaUTFPR/src/entities/Compromisso.java
+++ AgendaUTFPR/src/entities/Compromisso.java
@@ -7,6 +7,42 @@ public class Compromisso {
 	private String dataHoraFim;
 	private String local;
 	private String[] convidado;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getDataHoraInicio() {
+		return dataHoraInicio;
+	}
+	public void setDataHoraInicio(String dataHoraInicio) {
+		this.dataHoraInicio = dataHoraInicio;
+	}
+	public String getDataHoraFim() {
+		return dataHoraFim;
+	}
+	public void setDataHoraFim(String dataHoraFim) {
+		this.dataHoraFim = dataHoraFim;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String[] getConvidado() {
+		return convidado;
+	}
+	public void setConvidado(String[] convidado) {
+		this.convidado = convidado;
+	}
 	
 	
 	
