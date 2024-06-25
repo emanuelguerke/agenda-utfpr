b96991dde684e066a0591f03f1f061cdd3e6ef7b
João Gabriel Loureiro <146102111+LRGabrieldev@users.noreply.github.com>
2024-06-24 21:39:17 -0300

    Add files via upload

---
+++ AgendaUTFPR/src/entities/Imagem.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Imagem {
+	
+	private String nome;
+	
+	public Imagem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
